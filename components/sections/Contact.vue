893180970eb96e8551a126fb0a9685bd5fe49380
DCamron Dunlap <camron153@gmail.comgit config --global user.email camron153@gmail.com>
2024-06-06 06:39:14 -0400

    testing contact compent to make sure icons work as needed

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="mt-5">
+  <div class="mt-5 text-3xl">
     Contact me
+    <NuxtLink
+    to="https://github.com/dcamrondunlap"
+    target="_blank"
+    class=" hover:text-indigo-500 duration-300"
+    >
+      <Icon name="mdi:github" />
+    </NuxtLink>
   </div>
 </template>
